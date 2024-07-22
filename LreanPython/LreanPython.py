630a40710cfff23ac5933ca12a2d534d9f743c75
神煜森 <h18552897585@outlook.com>
2024-07-22 22:35:09 +0800

    数组区域更改

--- LreanPython/LreanPython.py
+++ LreanPython/LreanPython.py
@@ -12,7 +12,7 @@ GetType = type(Age);
 print(GetType);
 
 
-
+#Lrean Convert Area
 Age = "1";
 print(type(Age));
 a = int(Age);
@@ -21,7 +21,7 @@ b = float(Age);
 print(type(b));
 
 
-
+#Lrean String Area
 StringA = "Hello";
 StringB = "World";
 StringC = StringA+" "+StringB;
@@ -31,22 +31,45 @@ PersonLog = f"[Log]: This Person Input Age Is: {age} Year Old";
 print(PersonLog);
 
 
-
+#Lrean Array Area
 StrArray = ["A","B","C","D"];
 print(StrArray[0]);
 print(StrArray[1:3]);
 print(StrArray[:3]);
 print(StrArray[1:]);
+
 Info = f"[INFO]: Array Lenght: {len(StrArray)}";
 print(Info);
+
 x = [];
 y = list();
+
 x = ["a","b"];
 y = ["c","d"];
 StrArray = x+y;
+
 Info = f"[INFO]: Array Info: {StrArray[:]}";
 print(Info);
+
 StrArray = ["a","b"];
 StrArray.append("A");
 Info = f"[INFO]: Array Info: {StrArray[:]}";
+print(Info);
+
+StrArray.remove("A");
+Info = f"[INFO]: Array Info: {StrArray[:]}";
+print(Info);
+
+IntArray = [3,5,7,11];
+Info = f"[INFO]: Array Max Number: {max(IntArray)}";
+print(Info);
+Info = f"[INFO]: Array Min Number: {min(IntArray)}";
+print(Info);
+
+IntArray = [7,3];
+Info = f"[INFO]: Array Sum Number: {sum(IntArray)}";
+print(Info);
+
+IntArray = [7,3,11,5];
+Info = f"[INFO]: Array Sorted Number: {sorted(IntArray,reverse=False)}";
 print(Info);
