02715fe4418b20d758b6547dd67918877f3abf4b
神煜森 <h18552897585@outlook.com>
2024-07-22 22:12:29 +0800

    添加项目文件。

---
+++ LreanPython/LreanPython.py
@@ -0,0 +1,35 @@
+print("Hello,World!");
+
+
+
+Age = 114514;
+print(Age);
+
+
+
+Age = 114514;
+GetType = type(Age);
+print(GetType);
+
+
+
+Age = "1";
+print(type(Age));
+a = int(Age);
+print(type(a));
+b = float(Age);
+print(type(b));
+
+
+
+StringA = "Hello";
+StringB = "World";
+StringC = StringA+" "+StringB;
+print(StringC);
+age = 18;
+PersonLog = f"[INFO]: This Person Input Age Is: {age} Year Old";
+print(PersonLog);
+
+
+
+StrArray = ["A","B","C","D"];
